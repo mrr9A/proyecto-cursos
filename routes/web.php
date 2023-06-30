5e4405308711397fd6d851d3c706cf0149ed95b6
Luis Anberto <newprogrammer02@gmail.com>
2023-06-30 15:34:55 -0600

    iniciando con los reportes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\MatrizController;
 use App\Http\Controllers\ModalidadController;
 use App\Http\Controllers\PlanFormacionController;
 use App\Http\Controllers\PuestoController;
+use App\Http\Controllers\ReporteController;
 use App\Http\Controllers\SucursalesController;
 use App\Http\Controllers\TipoController;
 use App\Http\Controllers\TrabajoController;
@@ -53,6 +54,7 @@ Route::resource("cursosplanta/cursos/tipos", TipoController::class, ["names" => 
 Route::resource("cursosplanta/cursos", CursoController::class, ["names" => "cursos"]);
 Route::resource("cursosplanta/planes", PlanFormacionController::class, ["names" => "planes"]);
 Route::resource("cursosplanta/calificaciones", CalificacionController::class, ["names" => "calificaciones"]);
+Route::resource("cursosplanta/reportes", ReporteController::class, ["names" => "reportes"]);
 
 require __DIR__ . '/resource.php';
 
